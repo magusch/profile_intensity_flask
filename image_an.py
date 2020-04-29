cce6f8f4d865c8ea0a4c7c892002dff361b6aacc
magusch <magusch@gmail.com>
2020-04-29 23:59:02 +0300

    last

--- image_an.py
+++ image_an.py
@@ -17,9 +17,18 @@ def line_intensity(file):
 	
 	#profile_intensity=profile_rgb[:,0]*0.2126+profile_rgb[:,1]*0.7152+profile_rgb[:,2]*0.0722
 	profile_intensity=profile_rgb[:,0]+profile_rgb[:,1]+profile_rgb[:,2]
+	print()
+	I=profile_intensity[int(0.55*len(profile_intensity)):].max()
+	I0=profile_intensity[:int(0.45*len(profile_intensity))].max()
+	IdI0=I/I0
+	
 	plt.plot(range(len(profile_intensity)),profile_intensity)
+	plt.legend(['I/I0 = %s' %(IdI0)])
+	plt.title('Profile of image')
 	#plt.show()
 	filename=str(time.time())[4:10]+'.jpg'
+	plt.xlabel('Pixel distance')
+	plt.ylabel('Pixel intensity')
 	plt.savefig(file+ filename)
 	plt.close()
 	#plt.show()
